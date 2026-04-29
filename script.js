816512036a20b00ba49fb3740f5742ea0ef18b21
kushg1505 <kushg0259@gmail.com>
2026-04-29 16:14:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const targetDate = new Date("May 15, 2026 00:00:00").getTime();
+
+const timer = setInterval(function(){
+
+const now = new Date().getTime();
+const distance = targetDate - now;
+
+if(distance < 0){
+clearInterval(timer);
+document.getElementById("countdown").style.display="none";
+document.getElementById("enterBtn").classList.remove("hidden");
+return;
+}
+
+const days = Math.floor(distance / (1000*60*60*24));
+const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+const seconds = Math.floor((distance % (1000*60)) / 1000);
+
+document.getElementById("days").innerText = days;
+document.getElementById("hours").innerText = hours;
+document.getElementById("minutes").innerText = minutes;
+document.getElementById("seconds").innerText = seconds;
+
+},1000);
